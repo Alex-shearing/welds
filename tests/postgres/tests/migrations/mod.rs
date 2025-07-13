4238560b75ee4ea140f26e2bec9e1af4ff1e870a
aws404 <aws40404@gmail.com>
2025-07-13 20:33:49 +1000

    Fix tests

--- tests/postgres/tests/migrations/mod.rs
+++ tests/postgres/tests/migrations/mod.rs
@@ -458,7 +458,7 @@ fn should_be_able_to_create_a_pk_that_is_pk() {
 
         // down the migrations
         down(client, "test_create_primary_key_as_foreign_key_migration").await.unwrap();
-        down(client, "test_create_table_migration").await.unwrap()
+        down(client, "test_create_table_migration").await.unwrap();
 
         // make sure the table doesn't exist
         let table = find_table(None as Option<&str>, "blarf_fk", client)
