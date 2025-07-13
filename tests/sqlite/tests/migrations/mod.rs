1853bc541d94432b468668e980643748c8190fce
aws404 <aws40404@gmail.com>
2025-07-13 20:34:36 +1000

    tests

--- tests/sqlite/tests/migrations/mod.rs
+++ tests/sqlite/tests/migrations/mod.rs
@@ -7,7 +7,7 @@ use welds::migrations::MigrationStep;
 use welds::migrations::types::Type;
 use welds::migrations::types::OnDelete;
 use welds::migrations::{TableState, change_table, create_table};
-use welds::migrations::{down_last, up};
+use welds::migrations::{down_last, down, up};
 
 /************************************************
 * two migrations shouldn't have the same name
